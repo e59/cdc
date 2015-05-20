0d2baaddfde3d2852324aec31f2dc7825c01edda
Eduardo Marinho <e@calmaamigo.com>
2015-05-20 15:38:17 -0300

    'minpass'

--- src/Cdc/Definition/Helper/PasswordColumn.php
+++ src/Cdc/Definition/Helper/PasswordColumn.php
@@ -9,13 +9,15 @@ class PasswordColumn extends Helper {
     public static function def($args = array()) {
 
         $confirmation = A::get($args, 'confirmation', false);
+        $minLength = A::get($args, 'min', 0);
+        $maxLength = A::get($args, 'max', 32);
 
         $defaults = array(
             'type' => \Cdc\Definition::TYPE_COLUMN,
             \Cdc\Definition::TYPE_WIDGET => array(
                 'widget' => 'password',
                 'attributes' => array(
-                    'maxlength' => 32,
+                    'maxlength' => $maxLength,
                 ),
             ),
             \Cdc\Definition::OPERATION => array(
@@ -25,7 +27,7 @@ class PasswordColumn extends Helper {
                 DEFAULT_OPERATION => array(),
             ),
             \Cdc\Definition::TYPE_RULE => array(
-                array('\Cdc\Rule\Length', array(0, 32)),
+                'length' => array('\Cdc\Rule\Length', array($minLength, $maxLength)),
             ),
         );
 
