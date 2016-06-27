d90fe102605cac23f0b1dbf7fd31adab9452e2c2
Eduardo Marinho <e@calmaamigo.com>
2016-06-27 09:25:57 -0300

    cep

--- src/Cdc/Client/Cep.php
+++ src/Cdc/Client/Cep.php
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	 http://www.apache.org/licenses/LICENSE-2.0
+ *	 http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,56 +20,53 @@
  * @package Cdc
  * @subpackage Cdc_Client
  */
+
 /**
  * Obtém informações sobre um CEP diretamente da página dos correios.
  */
-namespace Cdc\Client;
-
-class Cep
+class Cdc_Client_Cep
 {
 
-    const RESULT_CEP = 'cep';
-    const RESULT_UF = 'uf';
-    const RESULT_CIDADE = 'cidade';
-    const RESULT_BAIRRO = 'bairro';
+    const RESULT_CEP      = 'cep';
+    const RESULT_UF       = 'uf';
+    const RESULT_CIDADE   = 'cidade';
+    const RESULT_BAIRRO   = 'bairro';
     const RESULT_ENDERECO = 'endereco';
 
     /**
      *
      * @param string $cep CEP
-     * @param mixed $extraParams Peculiaridades de cada forma de obtenção de endereço
      * @return array Array com os índices RESULT_* desta classe.
      */
-    public static function query($cep, $extraParams = array())
+    public static function query($cep)
     {
-        if ($extraParams) {
-            return Cdc_Client_Cep_Edne::query($cep, $extraParams);
-        }
-
 
         $curl_handle = curl_init();
 
-        curl_setopt($curl_handle, CURLOPT_URL, 'http://www.buscacep.correios.com.br/servicos/dnec/consultaLogradouroAction.do');
+        curl_setopt($curl_handle, CURLOPT_URL, 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm');
 
-        curl_setopt($curl_handle, CURLOPT_HEADER, false);
+        curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($curl_handle, CURLOPT_HEADER, true);
+        curl_setopt($curl_handle, CURLOPT_ENCODING, "");
         curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl_handle, CURLOPT_POST, true);
 
 
         curl_setopt($curl_handle, CURLOPT_POSTFIELDS, http_build_query(array(
-            'Metodo' => 'listaLogradouro',
-            'TipoConsulta' => 'cep',
-            'StartRow' => 1,
-            'EndRow' => 10,
-            'CEP' => $cep,
-        )));
-
+                    'relaxation'   => $cep,
+                    'tipoCEP'      => 'ALL',
+                    'semelhante'   => 'N',
+                )));
 
 
         $buffer = curl_exec($curl_handle);
         curl_close($curl_handle);
 
-        $document = new \DomDocument;
+        if (!$buffer) {
+            return false;
+        }
+
+        $document = new DomDocument;
 
         libxml_use_internal_errors(true); // Cheio de warning interpretando a página dos correios
         $document->loadHTML($buffer);
@@ -78,30 +75,38 @@ class Cep
         $elements = $document->getElementsByTagName('table');
 
         if (!$elements->length) {
-            return array();
+            return false;
+
         }
 
-        $endereco = $elements->item(2)->childNodes->item(0)->childNodes;
+
+        $endereco = $elements->item(0)->childNodes->item(1)->childNodes;
 
 
-        $logradouro = strip_tags($document->saveXML($endereco->item(0)));
-        $bairro = strip_tags($document->saveXML($endereco->item(2)));
-        $cidade = strip_tags($document->saveXML($endereco->item(4)));
-        $estado = strip_tags($document->saveXML($endereco->item(6)));
-        $cep_2 = preg_replace('#\D#', '', strip_tags($document->saveXML($endereco->item(8))));
+        $logradouro = trim(strip_tags($document->saveXML($endereco->item(0))));
+        $bairro     = trim(strip_tags($document->saveXML($endereco->item(2))));
+        $cidade_estado     = explode('/', trim(strip_tags($document->saveXML($endereco->item(4)))));
+        $cidade = trim(reset($cidade_estado));
+        $estado = trim(end($cidade_estado));
+        $cep_2      = trim(preg_replace('#\D#', '', strip_tags($document->saveXML($endereco->item(6)))));
 
-        if ($cep == $cep_2) {
+        if ($cep == $cep_2)
+        {
             $result = array(
-                self::RESULT_CEP => $cep,
-                self::RESULT_UF => $estado,
-                self::RESULT_CIDADE => $cidade,
-                self::RESULT_BAIRRO => $bairro,
+                self::RESULT_CEP      => $cep_2,
+                self::RESULT_UF       => $estado,
+                self::RESULT_CIDADE   => $cidade,
+                self::RESULT_BAIRRO   => $bairro,
                 self::RESULT_ENDERECO => $logradouro,
             );
 
+            foreach ($result as $key => $value) {
+                $result[$key] = preg_replace('/^[\pZ\pC]+|[\pZ\pC]+$/u', '', $value);
+            }
+
             return $result;
         }
-
-        return array();
     }
+
 }
+
